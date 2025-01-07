43027cebd2ac35a5a87e07e640759e2a90497ecf
Georgios Gantsios <ggantsios@uth.gr>
2025-01-07 20:30:06 +0200

    Tiled implementation. Huge decrease in accuracy. Runs correctly only for 5 bodies.

--- Code/nbody.cu
+++ Code/nbody.cu
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "timer.h"
+#define TILE_SIZE 1024
 // #include <omp.h>
 
 #define SOFTENING 1e-9f  /* Will guard against denormals */
-#define tolerance 0.01// 2 decimals
+#define tolerance 1// 2 decimals
 /* Macro for error checking in CUDA calls */
 #define CHECK_CUDA_ERROR(call)                                \
   do {                                                        \
@@ -43,37 +44,76 @@ void bodyForce(Body *p, float dt, int n) {
   }
 }
 
-__global__ void bodyForce_gpu(Body *p, float dt, int n) {
+// __global__ void bodyForce_gpu(Body *p, float dt, int n) {
+//   float distSqr;
+//   float invDist;
+//   float invDist3;
+//   int j;//, i;
+//   float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
+
+//   int index = blockIdx.x*blockDim.x + threadIdx.x;
+//   if(index < n){
+//     // Calculate constituted force
+//     for (j = 0; j < n; j++) {
+//         float dx = p[j].x - p[index].x;
+//         float dy = p[j].y - p[index].y;
+//         float dz = p[j].z - p[index].z;
+//         distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
+//         invDist = 1.0f / sqrtf(distSqr);
+//         invDist3 = invDist * invDist * invDist;
+
+//         Fx += dx * invDist3; Fy += dy * invDist3; Fz += dz * invDist3;
+//     }
+
+//     // Update velocity
+//     p[index].vx += dt*Fx; p[index].vy += dt*Fy; p[index].vz += dt*Fz;
+
+//     __syncthreads();
+//     // Update position
+//     p[index].x += p[index].vx*dt;
+//     p[index].y += p[index].vy*dt;
+//     p[index].z += p[index].vz*dt;
+//   }
+// }
+
+// bodyForce_gpu_tiled(dbuf_hor, dbuf_ver, dFx+offset, dFy+offset, dFz+offset, dt, TILE_SIZE)
+__global__ void bodyForce_gpu_tiled(Body *p_hor, Body *p_ver, float *Fx, float *Fy, float *Fz, float dt, int n) {
+
   float distSqr;
   float invDist;
   float invDist3;
   int j;//, i;
-  float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
 
   int index = blockIdx.x*blockDim.x + threadIdx.x;
   if(index < n){
     // Calculate constituted force
     for (j = 0; j < n; j++) {
-        float dx = p[j].x - p[index].x;
-        float dy = p[j].y - p[index].y;
-        float dz = p[j].z - p[index].z;
+        float dx = p_hor[j].x - p_ver[index].x;
+        float dy = p_hor[j].y - p_ver[index].y;
+        float dz = p_hor[j].z - p_ver[index].z;
         distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
         invDist = 1.0f / sqrtf(distSqr);
         invDist3 = invDist * invDist * invDist;
 
-        Fx += dx * invDist3; Fy += dy * invDist3; Fz += dz * invDist3;
+        Fx[index] += dx * invDist3; Fy[index] += dy * invDist3; Fz[index] += dz * invDist3;
     }
-
-    // Update velocity
-    p[index].vx += dt*Fx; p[index].vy += dt*Fy; p[index].vz += dt*Fz;
-
-    __syncthreads();
-    // Update position
-    p[index].x += p[index].vx*dt;
-    p[index].y += p[index].vy*dt;
-    p[index].z += p[index].vz*dt;
   }
 }
+
+__global__ void calculate_pos_vel(Body *p_ver, float *Fx, float *Fy, float *Fz, float dt, int n){
+  // Update velocity
+    int index = blockIdx.x*blockDim.x + threadIdx.x;
+    if(index < n){
+      p_ver[index].vx += dt*Fx[index]; p_ver[index].vy += dt*Fy[index]; p_ver[index].vz += dt*Fz[index];
+
+      // __syncthreads();
+      // Update position
+      p_ver[index].x += p_ver[index].vx*dt;
+      p_ver[index].y += p_ver[index].vy*dt;
+      p_ver[index].z += p_ver[index].vz*dt;
+    }
+}
+
 
 int check_correctness(Body *hbuf, Body *p, int nBodies){
   for (int i = 0; i < nBodies; i++) {
@@ -105,6 +145,8 @@ int main(const int argc, const char** argv) {
   const int nIters = 10;  // simulation iterations
   float *buf;
   Body *dbuf;
+  Body *dbuf_hor, *dbuf_ver;
+  float *dFx, *dFy, *dFz;
   Body *hbuf;
   Body *p;
   int bytes = nBodies*sizeof(Body);
@@ -120,6 +162,13 @@ int main(const int argc, const char** argv) {
   hbuf = (Body*)malloc(bytes);
   if(!hbuf) goto cleanup;
   CHECK_CUDA_ERROR(cudaMalloc((void**)&dbuf, bytes));
+  CHECK_CUDA_ERROR(cudaMalloc((void**)&dbuf_hor, TILE_SIZE*sizeof(Body)));
+  CHECK_CUDA_ERROR(cudaMalloc((void**)&dbuf_ver, TILE_SIZE*sizeof(Body)));
+  
+  CHECK_CUDA_ERROR(cudaMalloc((void**)&dFx, nBodies*nBodies*sizeof(float)));
+  CHECK_CUDA_ERROR(cudaMalloc((void**)&dFy, nBodies*nBodies*sizeof(float)));
+  CHECK_CUDA_ERROR(cudaMalloc((void**)&dFz, nBodies*nBodies*sizeof(float)));
+  
 
   p = (Body*)buf;
 
@@ -159,19 +208,45 @@ int main(const int argc, const char** argv) {
 
   cudaEventRecord(startCuda, 0);
   for (int iter = 1; iter <= nIters; iter++) {
-    bodyForce_gpu<<<(nBodies/1024)+extra_block, 1024>>>(dbuf, dt, nBodies);
-    cudaDeviceSynchronize();
-    cudaError_t err = cudaGetLastError();
-    if(err!=cudaSuccess){
-      printf("ERROR: %s, FILE: %s, LINE: %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
-      goto cleanup;
-    }
-    else{
-      printf("cudaGetLastError() == cudaSuccess!\n");
+    // bodyForce_gpu<<<(nBodies/1024)+extra_block, 1024>>>(dbuf, dt, nBodies);
+    CHECK_CUDA_ERROR(cudaMemset(dFx, 0, nBodies*nBodies*sizeof(float)));
+    CHECK_CUDA_ERROR(cudaMemset(dFy, 0, nBodies*nBodies*sizeof(float)));
+    CHECK_CUDA_ERROR(cudaMemset(dFz, 0, nBodies*nBodies*sizeof(float)));
+    for(int i = 0; i < nBodies; i += TILE_SIZE){
+      int j = 0;
+      int offset = i*nBodies+j;
+      CHECK_CUDA_ERROR(cudaMemcpy(dbuf_ver, dbuf + i, (TILE_SIZE*sizeof(Body) < bytes) ? TILE_SIZE*sizeof(Body): bytes, cudaMemcpyDeviceToDevice));
+      
+      for(j = 0; j < nBodies; j += TILE_SIZE){
+        CHECK_CUDA_ERROR(cudaMemcpy(dbuf_hor, dbuf + j, (TILE_SIZE*sizeof(Body) < bytes) ? TILE_SIZE*sizeof(Body): bytes, cudaMemcpyDeviceToDevice));
+        offset = i*nBodies+j;
+        bodyForce_gpu_tiled<<<(nBodies/TILE_SIZE) + (nBodies % TILE_SIZE > 0 ? 1 : 0), TILE_SIZE>>>(dbuf_hor, dbuf_ver, dFx+offset, dFy+offset, dFz+offset, dt, TILE_SIZE);
+        cudaDeviceSynchronize();
+        cudaError_t err = cudaGetLastError();
+        if(err!=cudaSuccess){
+          printf("ERROR: %s, FILE: %s, LINE: %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+          goto cleanup;
+        }
+        else{
+          printf("cudaGetLastError() == cudaSuccess! iter = %d, i = %d, j = %d\n", iter, i, j);
+        }
+      }
+      calculate_pos_vel<<<(nBodies/TILE_SIZE) + (nBodies % TILE_SIZE > 0 ? 1 : 0), TILE_SIZE>>>(dbuf_ver, dFx+offset, dFy+offset, dFz+offset, dt, TILE_SIZE);
+      cudaDeviceSynchronize();
+      cudaError_t err = cudaGetLastError();
+      if(err!=cudaSuccess){
+        printf("ERROR: %s, FILE: %s, LINE: %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+        goto cleanup;
+      }
+      else{
+        printf("cudaGetLastError() == cudaSuccess!\n");
+      }
+      CHECK_CUDA_ERROR(cudaMemcpy(dbuf+i, dbuf_ver, (TILE_SIZE*sizeof(Body) < bytes) ? TILE_SIZE*sizeof(Body): bytes , cudaMemcpyDeviceToDevice));
+      CHECK_CUDA_ERROR(cudaMemcpy(hbuf+i, dbuf_ver, (TILE_SIZE*sizeof(Body) < bytes) ? TILE_SIZE*sizeof(Body): bytes , cudaMemcpyDeviceToHost));
     }
   }
 
-  CHECK_CUDA_ERROR(cudaMemcpy(hbuf, dbuf, bytes, cudaMemcpyDeviceToHost));
+  // CHECK_CUDA_ERROR(cudaMemcpy(hbuf, dbuf, bytes, cudaMemcpyDeviceToHost));
   cudaEventRecord(stopCuda, 0);
   cudaEventSynchronize(stopCuda);
   cudaEventElapsedTime(&msec, startCuda, stopCuda);
@@ -189,6 +264,11 @@ int main(const int argc, const char** argv) {
   if (buf) free(buf);
   if (hbuf) free(hbuf);
   if (dbuf) cudaFree(dbuf);
+  if (dbuf_hor) cudaFree(dbuf_hor);
+  if (dbuf_ver) cudaFree(dbuf_ver);
+  if (dFx) cudaFree(dFx);
+  if (dFy) cudaFree(dFy);
+  if (dFz) cudaFree(dFz);
 
   CHECK_CUDA_ERROR(cudaEventDestroy(startCuda));
   CHECK_CUDA_ERROR(cudaEventDestroy(stopCuda));
